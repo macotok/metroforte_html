ae46244fbdfe7a602e6c78620a0777bcfcc82703
macotok <monochromer@r8.ucom.ne.jp>
2019-07-16 01:28:54 +0900

    リストのアニメーションの初期動作のみ

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,15 @@
 import Drawer from './Drawer';
 import Opening from './Opening';
 import pageLink from './pageLink';
+import $ from 'jquery';
 
 window.onload = () => {
-  const drawer = new Drawer();
-  drawer.openDrawer();
-  const opening = new Opening();
-  opening.event();
-  pageLink();
+  // const drawer = new Drawer();
+  // drawer.openDrawer();
+  // const opening = new Opening();
+  // opening.event();
+  // pageLink();
+  setTimeout(() => {
+    $('.fn-listAnimation').addClass('active');
+  }, 500);
 };
